769887758948db2fa4000290c9422e094f0868ae
Rilong <romgnatyuk@gmail.com>
2019-07-11 16:45:16 +0300

    Added some methods for model Project

--- app/Project.php
+++ app/Project.php
@@ -2,9 +2,48 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public $fillable = ['name', 'budget', 'start_date', 'finish_date', 'finished_date'];
+
+    public static function add(array $fields)
+    {
+        $new_project = new static();
+        $new_project->fill($fields);
+        $new_project->start_date = Carbon::now()->timestamp;
+        $new_project->save();
+        return $new_project;
+    }
+
+    public function change(array $fields)
+    {
+        $this->fill($fields);
+        return $this->save();
+    }
+
+    public function remove()
+    {
+         return $this->delete();
+    }
+
+    public function setBudget(float $budget)
+    {
+        $this->budget = $budget;
+        return $this->save();
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+        return $this->save();
+    }
+
+    public function finish()
+    {
+        $this->finished_date = Carbon::now()->timestamp;
+        return $this->save();
+    }
 }
