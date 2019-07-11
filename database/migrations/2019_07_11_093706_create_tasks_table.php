0d072dcd789258b20e18de65bbf9325d626a7554
Rilong <romgnatyuk@gmail.com>
2019-07-11 13:09:39 +0300

    Created migrations and models

---
+++ database/migrations/2019_07_11_093706_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 40);
+            $table->integer('project_id')->unsigned();
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->boolean('is_done')->default(0);
+            $table->timestamp('deadline_date')->nullable();
+            $table->timestamp('finished_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
