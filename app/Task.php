672dff818c25ba29e956e719e06459b723ec51cb
Rilong <romgnatyuk@gmail.com>
2019-07-12 16:13:40 +0300

    Added some methods for model task

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,50 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    public function project() {
+    public $fillable = ['title', 'deadline_date', 'finished_date'];
+
+    public function project()
+    {
         return $this->hasOne('App\Project', 'id', 'project_id');
     }
+
+    public static function add(array $fields)
+    {
+        $new_task = new static();
+        $new_task->fill($fields);
+        return $new_task->save();
+    }
+
+    public function change(array $fields)
+    {
+        $this->fill($fields);
+        return $this->save();
+    }
+
+    public function remove()
+    {
+        return $this->delete();
+    }
+
+    public function check() {
+        $this->is_done = true;
+        return $this->save();
+    }
+
+    public function uncheck() {
+        $this->is_done = false;
+        return $this->save();
+    }
+
+    public function toggleCheck() {
+        $this->is_done = !$this->is_done;
+        return $this->save();
+    }
+
+    public function setTitle(string $title) {
+        $this->title = $title;
+        return $this->save();
+    }
+
+
 }
