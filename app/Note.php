dd8585b1a425bd35aee0ab01bbc529837d8e1928
Rilong <romgnatyuk@gmail.com>
2019-07-12 16:26:50 +0300

    Added some methods for a Note model

--- app/Note.php
+++ app/Note.php
@@ -6,7 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
+
     public function project() {
         return $this->hasOne('App\Project', 'id', 'project_id');
+    }
+
+    public static function add(string $content)
+    {
+        $new_note = new static();
+        $new_note->content = $content;
+        $new_note->save();
+        return $new_note;
+    }
+
+    public function setContent(string $content)
+    {
+        $this->content = $content;
+        return $this->save();
+    }
+
+    public function remove()
+    {
+        return $this->delete();
     }
 }
