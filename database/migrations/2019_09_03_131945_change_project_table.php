9576721a2c20fef0d657c44771e6e8d065aedc45
Rilong <romgnatyuk@gmail.com>
2019-09-03 13:29:44 +0300

    Added a new migration for change projects table

---
+++ database/migrations/2019_09_03_131945_change_project_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Projects', function (Blueprint $table) {
+            $table->date('dateline_date')->after('start_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Projects', function (Blueprint $table) {
+            $table->dropColumn('dateline_date');
+        });
+    }
+}
