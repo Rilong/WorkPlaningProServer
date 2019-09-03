f80f1961dd6e99df603403b5ea3a948e4d5a7a2d
Rilong <romgnatyuk@gmail.com>
2019-09-03 12:32:34 +0300

    Replaced finish_date to finished_date

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -12,6 +12,6 @@ $factory->define(Project::class, function (Faker $faker) {
         'budget' => round($faker->numberBetween(5000, 15000), -3),
         'user_id' => 1,
         'start_date' => Carbon::now(),
-        'finish_date' => Carbon::now()->addMonth()
+        'finished_date' => Carbon::now()->addMonth()
     ];
 });
