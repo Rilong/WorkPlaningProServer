aabaf4cbf505969a7f3ac1bae7d319a7e5cb1b8e
Rilong <romgnatyuk@gmail.com>
2019-11-15 10:07:17 +0200

    Added description faker

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -9,6 +9,7 @@ use Faker\Generator as Faker;
 $factory->define(Project::class, function (Faker $faker) {
     return [
         'name' => $faker->words(1, true),
+        'description' => $faker->text(),
         'budget' => round($faker->numberBetween(5000, 15000), -3),
         'user_id' => 1,
         'start_date' => Carbon::now(),
